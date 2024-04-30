87b3c226c1c13d45d767ac0434cf12b65cc701b4
Daniel Méndez <145782489+Dmenmor2312@users.noreply.github.com>
2024-04-30 15:20:05 +0200

    Update Juego.java

--- NenRaya (clases)/Juego.java
+++ NenRaya (clases)/Juego.java
@@ -1,10 +1,6 @@
-package n_en_raya;
-//Hacer varios juegos al mismo tiempo
 public class Juego {
-
     public static void main(String[] args) {
-        JuegoNRaya juego1 = new JuegoNRaya();
-        
+        Njuegos j1 = new Njuegos();
+        j1.cambiarJuegos(j1.crearArrayJuegos());
     }
-
 }
