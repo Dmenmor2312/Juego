8f2a7019df7a4a2a71542127399cc4fff8410b4e
Daniel Méndez <145782489+Dmenmor2312@users.noreply.github.com>
2024-05-14 10:54:18 +0200

    Update Juego.java

--- NenRaya (clases)/Juego.java
+++ NenRaya (clases)/Juego.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Juego {
+
     public static void main(String[] args) {
-        Njuegos j1 = new Njuegos();
-        j1.cambiarJuegos(j1.crearArrayJuegos());
+        Juego j1 = new Juego();
+        j1.jugarNJuegos(crearArrayJuegos());
+    }
+
+    public static Njuegos[] crearArrayJuegos() {
+        Njuegos[] arrayJuegos;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("¿A cuantos juegos quieres jugar al mismo tiempo?");
+        int tamaño = leer.nextInt();
+        arrayJuegos = new Njuegos[tamaño];
+        for (int i = 0; i < arrayJuegos.length; i++) {
+            System.out.println("¿A qué juego quieres jugar? 1-NRaya 2-Aleatorio");
+            int juego = leer.nextInt();
+            if (juego == 1) {
+                arrayJuegos[i] = new JuegoNRaya();
+            } else if (juego == 2) {
+                arrayJuegos[i] = new JuegoAleatorio();
+            }
+        }
+        return arrayJuegos;
+    }
+
+    public static void jugarNJuegos(Njuegos [] arrayJuegos){
+        while(Njuegos.verificarJuegosTerminados(arrayJuegos)){
+            Njuegos.cambiarJuegos(arrayJuegos);
+        }
     }
 }
